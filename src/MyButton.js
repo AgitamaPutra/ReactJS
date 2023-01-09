4441fc982390a99d62a2628f7513c33e526149fc
aryaagitama87@gmail.com <aryaagitama87@gmail.com>
2023-01-09 20:58:25 +0800

    first

---
+++ src/MyButton.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import './index.css';
+
+class MyButton extends component {
+    render() {
+        return  <img src={process.env.PUBLIC_URL + '/logo192.png'} />;
+        
+    }
+}
